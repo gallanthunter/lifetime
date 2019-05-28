3fc4f3455538f3b107d828cf47890b9cba7d64d0
zhang zhijun <gallanthunter@163.com>
2019-05-28 22:03:55 +0800

    修改搜索框

---
+++ searchform.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * 功能:
+ * 参数：
+ * 作者: Zhang Zhijun
+ * 日期: 2019/5/28 21:20
+ */
+?>
+
+
+<form action="<?php bloginfo('siteurl'); ?>" id="searchform" method="get">
+    <input type="text" placeholder="请输入搜索内容" name="searchContent">
+    <button type="submit"></button>
+</form>
+
--- widget/about-x.php
+++ widget/about-x.php
@@ -15,8 +15,10 @@ class About_x extends WP_Widget
     
     function form($instance)
     {
-        $about_content = esc_attr($instance['about_content']);
         $title         = esc_attr($instance['title']);
+        $name          = esc_attr($instance['name']);
+        $alias         = esc_attr($instance['alias']);
+        $about_content = esc_attr($instance['about_content']);
         ?>
 
         <p>
@@ -24,6 +26,20 @@ class About_x extends WP_Widget
             <input type="text" size="40" value="<?php echo $title; ?>"
                    name="<?php echo $this->get_field_name('title'); ?>"
                    id="<?php echo $this->get_field_id('title'); ?>"/>
+        </p>
+
+        <p>
+            <label for="<?php echo $this->get_field_id('name'); ?>">网名:</label>
+            <input type="text" size="40" value="<?php echo $name; ?>"
+                   name="<?php echo $this->get_field_name('name'); ?>"
+                   id="<?php echo $this->get_field_id('name'); ?>"/>
+        </p>
+
+        <p>
+            <label for="<?php echo $this->get_field_id('alias'); ?>">别名:</label>
+            <input type="text" size="40" value="<?php echo $alias; ?>"
+                   name="<?php echo $this->get_field_name('alias'); ?>"
+                   id="<?php echo $this->get_field_id('alias'); ?>"/>
         </p>
 
         <p>
@@ -48,8 +64,10 @@ class About_x extends WP_Widget
     { // 输出显示在页面上
         extract($args);
         $id            = $instance['id'];
-        $about_content = apply_filters('widget_title', empty($instance['about_content']) ? __('') : $instance['about_content']);
         $title         = apply_filters('widget_title', empty($instance['title']) ? __('') : $instance['title']);
+        $name          = apply_filters('widget_title', empty($instance['name']) ? __('') : $instance['name']);
+        $alias         = apply_filters('widget_title', empty($instance['alias']) ? __('') : $instance['alias']);
+        $about_content = apply_filters('widget_title', empty($instance['about_content']) ? __('') : $instance['about_content']);
         ?>
 
 
@@ -59,7 +77,16 @@ class About_x extends WP_Widget
                     <img src="<?php echo get_template_directory_uri() . '/images/member_270x210.jpg'; ?>" alt="Member">
                 </div>
                 <div class="about-x-info">
-                    <?php echo html_entity_decode($about_content); ?>
+                    <h3 class="text-success">
+                        <?php echo html_entity_decode($name); ?>
+                    </h3>
+                    <h5 class="text-secondary">
+                        <?php echo html_entity_decode($alias); ?>
+                    </h5>
+                    <p class="text-secondary">
+                        <?php echo html_entity_decode($about_content); ?>
+                    </p>
+
                 </div>
 
                 <div class="social-touch">
